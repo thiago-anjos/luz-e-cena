a9aa47654752d23afa98ca7f872b042d1b8bcd94
thiago <thiago.w3c@gmail.com>
2025-11-24 13:05:09 -0300

    secao 05 - rodape

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieSection from "./components/MovieSection";
+import Newsletter from "./components/Newsletter";
 
 function App() {
   return (
     <>
       <Header />
-      <Banner src="./Banner.png" alt="Banner" />;
+      <Banner src="./banner.png" alt="Banner" />
       <MovieSection />
+      <Banner src="./combo.png" alt="Combo" />
+      <Newsletter />
+      <Footer />
     </>
   );
 }
